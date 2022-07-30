5a6a5e420bea04b9feb17a7e6e1d5e69120dde95
Nick Moody <Nicholasmoodz1543@gmail.com>
2022-07-30 16:01:36 -0700

    All tabs working, and connected to each other

---
+++ app.js
@@ -0,0 +1,14 @@
+//I wanted to make a drop down menu. this is the first thing that i'm going to do in jquery. 
+//here is my reference, it was a great tool in making this happen
+//https://www.youtube.com/watch?v=kxfXH0dxU70
+
+$(() => {
+    $('#links').hover(
+        () => {
+            $('ul.sub').slideDown(500)
+        },
+        () => {
+            $('ul.sub').slideUp(300)
+        }
+    )
+})
