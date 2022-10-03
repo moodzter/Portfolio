af2fff20e8ab9a1dcd9c5ecb259104c5e495b759
Nick Moody <Nicholasmoodz1543@gmail.com>
2022-10-03 01:39:04 -0700

    added New projects

--- projects/app.js
+++ projects/app.js
@@ -14,7 +14,7 @@ $(() => {
 
 
 const addText1 = () => {
-    let description1 = $('<p>').text(`Above you can see website homepage I designed, with the goal of having users able to share and see other people's art. Included is the use of buttons and text boxes for including your name.`)
+    let description1 = $('<p>').text(`In Arts R Us I used an express-style app with a mongo database to build a fully-functioning CRUD based application. I used Express, Html, Mongo, Javascript, and CSS to accomplish this.`)
 
     $('#p1').append(description1)
     $('#project1Button').prop('disabled', true)
@@ -22,7 +22,7 @@ const addText1 = () => {
     }
 
 const addText2  = () => {
-    let description2 = $('<p>').text(`Above is another home page I designed for a specific historical artist named Fridah Kahlo. Included is a nav bar with different people related to her and pieces of her art and descriptions of them. There are buttons you can use to "like" the art pieces.`)
+    let description2 = $('<p>').text(`In Comic World I collaborated with another partner to accomplish a fully functioning CRUD based application using ReactJS for our front-end and MongoDB as our back-end. I successfully implented a sort feature which makes a unique array of names with no duplicates based on JSON information from an API.`)
 
     $('#p2').append(description2)
     $('#project2Button').prop('disabled', true)
@@ -30,7 +30,7 @@ const addText2  = () => {
     }
 
 const addText3  = () => {
-    let description3 = $('<p>').text(`Here is a business page I designed for the use of buying and selling famous art pieces. This page is designed for buying, and I made use of javascript to have the descriptions pop up after you click them.`)
+    let description3 = $('<p>').text(`Keyboard Part-Picker is a CRUD Based application where users can build their own keyboard, or select a default one from a list they can search through. I built this with two other teammates, and we used Django as our back-end framework, react as our front-end, and PostgreSQL as our backend server. `)
     
     $('#p3').append(description3)
     $('#project3Button').prop('disabled', true)
